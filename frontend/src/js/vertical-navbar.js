177561b6000cf8511ebfd2f0cc1227942b33b155
eimbens <e.imbens@gmail.com>
2024-02-29 15:28:40 +0100

    Put pages in wails framework

--- frontend/src/js/vertical-navbar.js
+++ frontend/src/js/vertical-navbar.js
@@ -8,30 +8,30 @@ class Navbar extends HTMLElement {
     <div id="header">
       <div id="header-logo">
           <a href="/home/" class="logo-name">
-              <img src="/Resources/App/Static/logoTeamA-transformed.png" alt="logo"></img>
+              <img src="./src/assets/images/logoTeamA-transformed.png" alt="logo"></img>
               <div id="header-name">
                   <h1>Little Brother</h1>
               </div>
           </a>
       </div>
       <div id="header-settings">
-          <a href="/settings/"><p><span class="material-symbols-outlined">settings</span><span>Settings</span></p></a>
+          <a href="./settings.html"><p><span class="material-symbols-outlined">settings</span><span>Settings</span></p></a>
       </div>
     </div> 
     <div id="left-nav">
-        <a href="/home/">
+        <a href="./index.html">
             <p><span class="material-symbols-outlined">home</span><span class="nav-item">Home</span></p>
         </a>
-        <a href="/dashboard/">
+        <a href="./dashboard.html">
             <p><span class="material-symbols-outlined">monitoring</span><span class="nav-item">Dashboard</span></p>
         </a>
-        <a href="/issues/">
+        <a href="./issues.html">
             <p><span class="material-symbols-outlined">security</span><span class="nav-item">Issues</span></p>
         </a>
-        <a href="/integration/">
+        <a href="./integration.html">
             <p><span class="material-symbols-outlined">integration_instructions</span><span class="nav-item">Integration</span></p>
         </a>
-        <a href="/about/">
+        <a href="./about.html">
             <p><span class="material-symbols-outlined">info</span><span class="nav-item">About</span></p>
         </a>
     </div>
