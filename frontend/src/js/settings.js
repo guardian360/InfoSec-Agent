b17713724340a7ea30d37cd1dbcd27d510473b0a
eimbens <e.imbens@gmail.com>
2024-03-01 11:52:02 +0100

    Start implementing settings page

--- frontend/src/js/settings.js
+++ frontend/src/js/settings.js
@@ -1,6 +1,15 @@
 function toggleDarkMode() {
     console.log("toggle dark mode");
+    var darkModeSwitch = document.getElementById("dark-mode-switch");
 
-    
+    if (darkModeSwitch.checked == true) {
+        document.documentElement.style.setProperty('--background-color', '#333');
+        document.documentElement.style.setProperty('--dark-text', '#d4d4d4');
+    }
+    else {
+        document.documentElement.style.setProperty('--background-color', '#ffffff');
+        document.documentElement.style.setProperty('--dark-text', '#333');
+    }
+
     
 }
