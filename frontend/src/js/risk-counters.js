fc513f427882e3361916f76b00804d85162bdc98
LarsSlijkoord <l.x.slijkoord@students.uu.nl>
2024-03-01 22:12:10 +0100

    most resizing issues fixed on dashboard

--- frontend/src/js/risk-counters.js
+++ frontend/src/js/risk-counters.js
@@ -1,7 +1,11 @@
+var highRiskColor = getComputedStyle(document.documentElement).getPropertyValue('--highRiskColor');
+var mediumRiskColor = getComputedStyle(document.documentElement).getPropertyValue('--mediumRiskColor');
+var lowRiskColor = getComputedStyle(document.documentElement).getPropertyValue('--lowRiskColor');
+var noRiskColor = getComputedStyle(document.documentElement).getPropertyValue('--noRiskColor');
 // this file should contain code to put the correct count for each risk assessment.
 
-var allHighRisks = [1,2,3,4,5,6,5];
-var allMediumRisks = [1,2,3,4,5,6,3];
+var allHighRisks = [1,2,3,4,5,6,2];
+var allMediumRisks = [1,2,3,4,5,6,0];
 var allLowRisks = [1,2,3,4,5,6,2];
 var allNoRisks = [1,2,3,4,5,6,4];
 
@@ -12,8 +16,33 @@ var graphShowMediumRisks = true;
 var graphShowLowRisks = true;
 var graphShowNoRisks = true;
 
+var lastHighRisk = allHighRisks.slice(-1)[0];
+var lastMediumRisk = allMediumRisks.slice(-1)[0];
+var lastLowRisk = allLowRisks.slice(-1)[0];
+var lastnoRisk = allNoRisks.slice(-1)[0];
+
 // change counters according to collected data
-document.getElementById("high-risk-counter").innerHTML = allHighRisks.slice(-1)[0];
-document.getElementById("medium-risk-counter").innerHTML = allMediumRisks.slice(-1)[0];
-document.getElementById("low-risk-counter").innerHTML = allLowRisks.slice(-1)[0];
-document.getElementById("no-risk-counter").innerHTML = allNoRisks.slice(-1)[0];
+document.getElementById("high-risk-counter").innerHTML = lastHighRisk;
+document.getElementById("medium-risk-counter").innerHTML = lastMediumRisk;
+document.getElementById("low-risk-counter").innerHTML = lastLowRisk;
+document.getElementById("no-risk-counter").innerHTML = lastnoRisk;
+
+if (lastHighRisk > 1) {
+    document.getElementById("security-status").innerHTML = "Critical";
+    document.getElementById("security-status").style.backgroundColor = highRiskColor;
+    document.getElementById("security-status").style.color = "rgb(255, 255, 255)";
+} else if (lastMediumRisk > 1) {
+    document.getElementById("security-status").innerHTML = "Medium concern";
+    document.getElementById("security-status").style.backgroundColor = mediumRiskColor; 
+    document.getElementById("security-status").style.color = "rgb(255, 255, 255)";
+} else if (lastLowRisk > 1) {
+    document.getElementById("security-status").innerHTML = "Light concern";
+    document.getElementById("security-status").style.backgroundColor = lowRiskColor;
+    document.getElementById("security-status").style.color = "rgb(0, 0, 0)";  
+} else {
+    document.getElementById("security-status").innerHTML = "Safe";
+    document.getElementById("security-status").style.backgroundColor = noRiskColor;
+    document.getElementById("security-status").style.color = "rgb(0, 0, 0)";  
+}
+
+document.getElementById("graph-interval").max = allNoRisks.length;
