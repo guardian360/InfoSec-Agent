ff229005e0950c1ab3af17bedd804a5bc74d9c8a
Olaf Boekholt <olafboekholt@gmail.com>
2024-03-08 11:11:08 +0100

    Check struct, change smb to new output format

--- checks/smb_check.go
+++ checks/smb_check.go
@@ -7,27 +7,36 @@ import (
 )
 
 // SmbCheck checks whether SMB1 and SMB2 are enabled
-func SmbCheck() {
-	smbEnabled("SMB1")
-	smbEnabled("SMB2")
+func SmbCheck() Check {
+	smb1, err := smbEnabled("SMB1")
+
+	if err != nil {
+		return newCheckError("smb", err)
+	}
+	smb2, err := smbEnabled("SMB2")
+
+	if err != nil {
+		return newCheckError("smb", err)
+	}
+
+	return newCheckResult("smb", []string{smb1, smb2})
 }
 
 // Check whether specified SMB protocol is enabled
-func smbEnabled(smb string) {
+func smbEnabled(smb string) (string, error) {
 	// Format command
 	command := fmt.Sprintf("Get-SmbServerConfiguration | Select-Object Enable%sProtocol", smb)
 
 	output, err := exec.Command("powershell", command).Output()
 	if err != nil {
-		fmt.Println("Error executing command:", err)
-		return
+		return "", err
 	}
 
 	outputString := strings.Split(string(output), "\r\n")
 	line := strings.TrimSpace(outputString[3])
 	if line == "True" {
-		fmt.Println(smb, "is enabled")
-	} else {
-		fmt.Println(smb, "is not enabled")
+		return smb + ": enabled", nil
 	}
+
+	return smb + ": not enabled", nil
 }
