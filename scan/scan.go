4ac028739a4fae4a5e388064a09d53015677bb09
Olaf Boekholt <olafboekholt@gmail.com>
2024-03-08 11:29:38 +0100

    scan package that writes check output to file

---
+++ scan/scan.go
@@ -0,0 +1,42 @@
+package scan
+
+import (
+	"InfoSec-Agent/checks"
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+func Scan() {
+	// Run all checks
+	smb := checks.SmbCheck()
+	secureBoot := checks.SecureBoot()
+
+	// Combine results
+	checkResults := []checks.Check{
+		smb,
+		secureBoot,
+	}
+
+	// Serialize check results to JSON
+	jsonData, err := json.Marshal(checkResults)
+	if err != nil {
+		fmt.Println("Error marshalling JSON:", err)
+		return
+	}
+	fmt.Println(string(jsonData))
+
+	// Write JSON data to a file
+	file, err := os.Create("output.json")
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
+	}
+	defer file.Close()
+
+	_, err = file.Write(jsonData)
+	if err != nil {
+		fmt.Println("Error writing JSON data to file:", err)
+		return
+	}
+}
