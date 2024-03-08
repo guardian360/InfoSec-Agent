18f6663b56cedda15f3a6dd0a8a701aac7a337c9
Olaf Boekholt <olafboekholt@gmail.com>
2024-03-08 12:43:28 +0100

    format errors

--- checks/checks.go
+++ checks/checks.go
@@ -1,9 +1,12 @@
 package checks
 
+import "fmt"
+
 type Check struct {
-	Id     string
-	Result []string
-	Error  error
+	Id       string
+	Result   []string
+	Error    error `json:"-"` // Don't serialize error field to JSON
+	ErrorMSG string
 }
 
 func newCheckResult(id string, result ...string) Check {
@@ -11,5 +14,10 @@ func newCheckResult(id string, result ...string) Check {
 }
 
 func newCheckError(id string, err error) Check {
-	return Check{Id: id, Error: err}
+	return Check{Id: id, Error: err, ErrorMSG: err.Error()}
+}
+
+func newCheckErrorf(id string, message string, err error) Check {
+	formatErr := fmt.Errorf(message+": %w", err)
+	return Check{Id: id, Error: formatErr, ErrorMSG: formatErr.Error()}
 }
