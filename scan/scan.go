e9c8f0dcea480d9f3a046d0baee80dca6c6c9b97
Olaf Boekholt <olafboekholt@gmail.com>
2024-03-08 16:25:15 +0100

    comment writing json to file

--- scan/scan.go
+++ scan/scan.go
@@ -4,7 +4,6 @@ import (
 	"InfoSec-Agent/checks"
 	"encoding/json"
 	"fmt"
-	"os"
 )
 
 func Scan() {
@@ -62,17 +61,17 @@ func Scan() {
 	}
 	fmt.Println(string(jsonData))
 
-	// Write JSON data to a file
-	file, err := os.Create("checks.json")
-	if err != nil {
-		fmt.Println("Error creating file:", err)
-		return
-	}
-	defer file.Close()
-
-	_, err = file.Write(jsonData)
-	if err != nil {
-		fmt.Println("Error writing JSON data to file:", err)
-		return
-	}
+	//// Write JSON data to a file
+	//file, err := os.Create("checks.json")
+	//if err != nil {
+	//	fmt.Println("Error creating file:", err)
+	//	return
+	//}
+	//defer file.Close()
+	//
+	//_, err = file.Write(jsonData)
+	//if err != nil {
+	//	fmt.Println("Error writing JSON data to file:", err)
+	//	return
+	//}
 }
