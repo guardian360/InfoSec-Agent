3b2b5864db0ed32272042499321e4d4d690bcff7
Knoop,K. (Kevin) <kefkefknoop@gmail.com>
2024-03-11 10:56:39 +0100

    ports.go documentation

--- checks/ports.go
+++ checks/ports.go
@@ -1,3 +1,8 @@
+// Package checks implements different security/privacy checks
+//
+// Exported function(s): PasswordManager, WindowsDefender, LastPasswordChange, LoginMethod, Permission, Bluetooth,
+// OpenPorts, WindowsOutdated, SecureBoot, SmbCheck, Startup, GuestAccount, UACCheck, RemoteDesktopCheck,
+// ExternalDevices, NetworkSharing
 package checks
 
 import (
@@ -7,21 +12,29 @@ import (
 	"strings"
 )
 
+// OpenPorts checks for open ports and the processes that are using them
+//
+// Parameters: _
+//
+// Returns: A list of open ports and the processes that are using them
 func OpenPorts() Check {
+	// Regular expression to clean up multiple spaces in the output
 	re := regexp.MustCompile("  +")
 
-	// Get pids and names of all processes
+	// Get process ids (pids) and names of all processes
 	output, err := exec.Command("tasklist").Output()
 	if err != nil {
 		return newCheckErrorf("OpenPorts", "error running tasklist", err)
 	}
 	tasklist := strings.Split(string(output), "\r\n")
 
-	// Map of pids to process names
+	// Map each pid to its process name
 	pids := make(map[string]string)
 	for _, line := range tasklist[3 : len(tasklist)-1] {
 		words := re.Split(line, 2)[:2]
+		// Extract the process name
 		words[1] = strings.Fields(words[1])[0]
+		// Map the pid to its process name
 		pids[words[1]] = words[0]
 	}
 
@@ -44,9 +57,10 @@ func OpenPorts() Check {
 			continue
 		}
 
+		// Extract the port from the ip
 		port := ip[len(ip)-1]
 
-		// Get pid from line
+		// Get pid from line depending on the protocol used (TCP or UDP)
 		var pid string
 		if words[0] == "TCP" {
 			pid = words[4]
