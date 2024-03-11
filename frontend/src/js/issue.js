27b3b9940489f98c94a9f18bc0ecaa91c851e299
eimbens <e.imbens@gmail.com>
2024-03-11 16:38:08 +0100

    Add documentation

--- frontend/src/js/issue.js
+++ frontend/src/js/issue.js
@@ -1,31 +1,34 @@
 import data from "../database.json" assert { type: "json" };
 import { openIssuesPage } from "./issues.js";
 
-let solutionStepCounter = 0;
+let stepCounter = 0;
 
+// Update contents of solution guide
 function updateSolutionStep(solution, screenshots, stepCounter) {
     const solutionStep = document.getElementById("solution-text");
     solutionStep.innerHTML = solution[stepCounter];
     document.getElementById("step-screenshot").src = screenshots[stepCounter];
 }
 
+// Go to next step of solution guide
 function nextSolutionStep(solution, screenshots) {
-    if (solutionStepCounter < solution.length - 1) {
-        solutionStepCounter++;
-        updateSolutionStep(solution, screenshots, solutionStepCounter);
+    if (stepCounter < solution.length - 1) {
+        stepCounter++;
+        updateSolutionStep(solution, screenshots, stepCounter);
     }
 }
 
+// Go to previous step of solution guide
 function previousSolutionStep(solution, screenshots) {
-    if (solutionStepCounter > 0) {
-        solutionStepCounter--;
-        updateSolutionStep(solution, screenshots, solutionStepCounter);
+    if (stepCounter > 0) {
+        stepCounter--;
+        updateSolutionStep(solution, screenshots, stepCounter);
     }
 }
 
+// Load the content of the issue page
 export function openIssuePage(issueId) {
-    console.log("opened issue page: " + issueId);
-
+    stepCounter = 0;
     const currentIssue = data.find((element) => element.Name === issueId);
     const pageContents = document.getElementById("page-contents");
     pageContents.innerHTML = `
@@ -35,7 +38,7 @@ export function openIssuePage(issueId) {
             <p>${currentIssue.Information}</p>
             <h2>Solution</h2>
             <div id="issue-solution">
-                <p id="solution-text">${currentIssue.Solution[solutionStepCounter]}</p>
+                <p id="solution-text">${currentIssue.Solution[stepCounter]}</p>
                 <img style='display:block; width:500px;height:auto' id="step-screenshot"></img>
                 <div id="solution-buttons">
                     <div id="button-box">
@@ -49,9 +52,8 @@ export function openIssuePage(issueId) {
     `;
 
     try {
-        document.getElementById("step-screenshot").src = currentIssue.Screenshots[solutionStepCounter];
+        document.getElementById("step-screenshot").src = currentIssue.Screenshots[stepCounter];
     } catch (error) { }
-    
 
     document.getElementById("next-button").addEventListener("click", () => nextSolutionStep(currentIssue.Solution, currentIssue.Screenshots));
     document.getElementById("previous-button").addEventListener("click", () => previousSolutionStep(currentIssue.Solution, currentIssue.Screenshots));
