1a3ed43397776386f3cbbe997dca9af3c72ca519
LarsSlijkoord <l.x.slijkoord@students.uu.nl>
2024-03-18 21:02:16 +0100

    refactored reporting page code

--- reporting-page/frontend/src/js/home.js
+++ reporting-page/frontend/src/js/home.js
@@ -1,46 +1,47 @@
 import * as piechart from "./piechart";
 
+/** Load the content of the Home page */
 function openHomePage() {
-    document.getElementById("page-contents").innerHTML = `
-    <div class="container-data">       
-        <div class="data-column risk-counters">     
-            <div class="data-column piechart">
-                <canvas id="pieChart"></canvas>
-            </div>
-        </div>
-        <div class="data-column issue-buttons">
-            <H2>You have some issues you can fix. 
-                To start resolving a issue either navigate to the issues page, or pick a suggested issue below
-            </H2>
-            <a class="issue-button">Suggested Issue</a>
-            <a class="issue-button">Quick Fix</a>
-        </div>
+  document.getElementById("page-contents").innerHTML = `
+  <div class="container-data">       
+    <div class="data-column risk-counters">     
+      <div class="data-column piechart">
+        <canvas id="pieChart"></canvas>
+      </div>
     </div>
-    <h2 id="title-medals">Medals</h2>
-    <div class="container">  
-        <div class="medal-layout">
-            <img src="src/assets/images/img_medal1.jpg" alt="Photo of medal">
-            <p id="medal-name"> Medal 1</p>
-            <p> 01-03-2024</p>
-        </div>
-        <div class="medal-layout">
-            <img src="src/assets/images/img_medal1.jpg" alt="Photo of medal">
-            <p id="medal-name"> Medal 2</p>
-            <p> 01-03-2024</p>
-        </div>
-        <div class="medal-layout">
-            <img src="src/assets/images/img_medal1.jpg" alt="Photo of medal">
-            <p id="medal-name"> Medal 3</p>
-            <p> 01-03-2024</p>
-        </div><div class="medal-layout">
-            <img src="src/assets/images/img_medal1.jpg" alt="Photo of medal">
-            <p id="medal-name"> Medal 1</p>
-            <p> 01-03-2024</p>
-        </div>
+    <div class="data-column issue-buttons">
+      <H2>You have some issues you can fix. 
+        To start resolving a issue either navigate to the issues page, or pick a suggested issue below
+      </H2>
+      <a class="issue-button">Suggested Issue</a>
+      <a class="issue-button">Quick Fix</a>
     </div>
-    `;
+  </div>
+  <h2 class="title-medals">Medals</h2>
+  <div class="container">  
+    <div class="medal-layout">
+      <img src="src/assets/images/img_medal1.jpg" alt="Photo of medal">
+      <p class="medal-name"> Medal 1</p>
+      <p> 01-03-2024</p>
+    </div>
+    <div class="medal-layout">
+      <img src="src/assets/images/img_medal1.jpg" alt="Photo of medal">
+      <p class="medal-name"> Medal 2</p>
+      <p> 01-03-2024</p>
+    </div>
+    <div class="medal-layout">
+      <img src="src/assets/images/img_medal1.jpg" alt="Photo of medal">
+      <p class="medal-name"> Medal 3</p>
+      <p> 01-03-2024</p>
+    </div><div class="medal-layout">
+      <img src="src/assets/images/img_medal1.jpg" alt="Photo of medal">
+      <p class="medal-name"> Medal 1</p>
+      <p> 01-03-2024</p>
+    </div>
+  </div>
+  `;  
 
-    CreatePieChart();
+  CreatePieChart();
 }
 
 document.getElementById("logo-button").addEventListener("click", () => openHomePage());
@@ -50,13 +51,14 @@ document.getElementById("home-button").addEventListener("click", () => openHomeP
 
 // Reusable snippit for other files
 let pieChart;
+
+/** Creates a pie chart for risks */
 function CreatePieChart() {
-    pieChart = new Chart("pieChart", {
-        type: "doughnut",
-        data: piechart.GetData()
-        ,
-        options: piechart.GetOptions()
-      });
+  pieChart = new Chart("pieChart", {
+    type: "doughnut",
+    data: piechart.GetData(),
+    options: piechart.GetOptions()
+  });
 }
 
 //#endregion
--- reporting-page/frontend/src/js/settings.js
+++ reporting-page/frontend/src/js/settings.js
@@ -1,29 +1,30 @@
+/** Load the content of the Settings page */
 function openSettingsPage() {
-    document.getElementById("page-contents").innerHTML = `
-    <div class="setting" id="dark-mode">
-      <span class="setting-description">Dark mode</span>
-      <label class="switch">
-        <input type="checkbox" id="dark-mode-switch">
-        <span class="slider round"></span>
-      </label>
-    </div>
-    <hr class="solid">
-    <div class="setting">
-      <span class="setting-description">Other setting</span>
-      <label class="switch">
-        <input type="checkbox">
-        <span class="slider round"></span>
-      </label>
-    </div>
-    <hr class="solid">
-    <div class="setting">
-      <span class="setting-description">Other setting</span>
-      <label class="switch">
-        <input type="checkbox">
-        <span class="slider round"></span>
-      </label>
-    </div>
-    `;
+  document.getElementById("page-contents").innerHTML = `
+  <div class="setting dark-mode">
+    <span class="setting-description">Dark mode</span>
+    <label class="switch">
+      <input type="checkbox" id="dark-mode-switch">
+      <span class="slider round"></span>
+    </label>
+  </div>
+  <hr class="solid">
+  <div class="setting">
+    <span class="setting-description">Other setting</span>
+    <label class="switch">
+      <input type="checkbox">
+      <span class="slider round"></span>
+    </label>
+  </div>
+  <hr class="solid">
+  <div class="setting">
+    <span class="setting-description">Other setting</span>
+    <label class="switch">
+      <input type="checkbox">
+      <span class="slider round"></span>
+    </label>
+  </div>
+  `;
 }
 
 document.getElementById("settings-button").addEventListener("click", () => openSettingsPage());
