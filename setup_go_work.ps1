2c5fe27d60f4a83e3e87961ae3f54b7478a4a5b0
Olaf Boekholt <olafboekholt@gmail.com>
2024-03-22 09:36:02 +0100

    remove ps1, only bat script is enough

--- setup_go_work.ps1
+++
@@ -1,9 +0,0 @@
-# Simple script to setup go work environment
-# This script is intended to be run from the root of the project
-# It will initialize the go work environment, and then use the current directory and the reporting-page directory
-# Usage: .\setup_go_work.ps1
-
-go work init
-go work use .
-go work use .\reporting-page
-go work sync
