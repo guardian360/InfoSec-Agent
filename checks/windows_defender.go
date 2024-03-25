02bcf5b699d25ae0ad5a1c82934a91c6bae5fcd4
Knoop,K. (Kevin) <kefkefknoop@gmail.com>
2024-03-25 11:54:46 +0100

    Unit tests for windows_defender.go

--- checks/windows_defender.go
+++ checks/windows_defender.go
@@ -2,7 +2,6 @@ package checks
 
 import (
 	"github.com/InfoSec-Agent/InfoSec-Agent/registrymock"
-	"golang.org/x/sys/windows/registry"
 )
 
 // WindowsDefender checks if the Windows Defender is enabled and if the periodic scan is enabled
@@ -10,9 +9,10 @@ import (
 // Parameters: _
 //
 // Returns: If Windows Defender and periodic scan are enabled/disabled
-func WindowsDefender() Check {
+func WindowsDefender(scanKey registrymock.RegistryKey, defenderKey registrymock.RegistryKey) Check {
 	// Open the Windows Defender registry key
-	windowsDefenderKey, err := registrymock.OpenRegistryKey(registrymock.NewRegistryKeyWrapper(registry.LOCAL_MACHINE), `SOFTWARE\Microsoft\Windows Defender`)
+	//windowsDefenderKey, err := registrymock.OpenRegistryKey(registrymock.NewRegistryKeyWrapper(registry.LOCAL_MACHINE), `SOFTWARE\Microsoft\Windows Defender`)
+	windowsDefenderKey, err := registrymock.OpenRegistryKey(scanKey, `SOFTWARE\Microsoft\Windows Defender`)
 	if err != nil {
 		return NewCheckErrorf("WindowsDefender", "error opening registry key", err)
 	}
@@ -20,8 +20,9 @@ func WindowsDefender() Check {
 	defer registrymock.CloseRegistryKey(windowsDefenderKey)
 
 	// Open the Windows Defender real-time protection registry key, representing the periodic scan
-	realTimeKey, err := registrymock.OpenRegistryKey(registrymock.NewRegistryKeyWrapper(registry.LOCAL_MACHINE),
-		`SOFTWARE\Microsoft\Windows Defender\Real-Time Protection`)
+	//realTimeKey, err := registrymock.OpenRegistryKey(registrymock.NewRegistryKeyWrapper(registry.LOCAL_MACHINE),
+	//	`SOFTWARE\Microsoft\Windows Defender\Real-Time Protection`)
+	realTimeKey, err := registrymock.OpenRegistryKey(defenderKey, `SOFTWARE\Microsoft\Windows Defender\Real-Time Protection`)
 	if err != nil {
 		return NewCheckErrorf("WindowsDefender", "error opening registry key", err)
 	}
