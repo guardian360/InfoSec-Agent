66c1b9a43e449c5156b2957bc3e2e70863a88a2c
Knoop,K. (Kevin) <kefkefknoop@gmail.com>
2024-03-25 12:02:22 +0100

    Removed redundant comments

--- checks/windows_defender.go
+++ checks/windows_defender.go
@@ -11,7 +11,6 @@ import (
 // Returns: If Windows Defender and periodic scan are enabled/disabled
 func WindowsDefender(scanKey registrymock.RegistryKey, defenderKey registrymock.RegistryKey) Check {
 	// Open the Windows Defender registry key
-	//windowsDefenderKey, err := registrymock.OpenRegistryKey(registrymock.NewRegistryKeyWrapper(registry.LOCAL_MACHINE), `SOFTWARE\Microsoft\Windows Defender`)
 	windowsDefenderKey, err := registrymock.OpenRegistryKey(scanKey, `SOFTWARE\Microsoft\Windows Defender`)
 	if err != nil {
 		return NewCheckErrorf("WindowsDefender", "error opening registry key", err)
@@ -20,8 +19,6 @@ func WindowsDefender(scanKey registrymock.RegistryKey, defenderKey registrymock.
 	defer registrymock.CloseRegistryKey(windowsDefenderKey)
 
 	// Open the Windows Defender real-time protection registry key, representing the periodic scan
-	//realTimeKey, err := registrymock.OpenRegistryKey(registrymock.NewRegistryKeyWrapper(registry.LOCAL_MACHINE),
-	//	`SOFTWARE\Microsoft\Windows Defender\Real-Time Protection`)
 	realTimeKey, err := registrymock.OpenRegistryKey(defenderKey, `SOFTWARE\Microsoft\Windows Defender\Real-Time Protection`)
 	if err != nil {
 		return NewCheckErrorf("WindowsDefender", "error opening registry key", err)
