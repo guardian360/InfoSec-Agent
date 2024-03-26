59954056a154ba503e5de35acec60cfc0413ceab
SjaakRR <sjaak0110@gmail.com>
2024-03-26 17:06:39 +0100

    Fixed tests

--- checks/windows_loginmethod_test.go
+++ checks/windows_loginmethod_test.go
@@ -23,18 +23,21 @@ func TestLoginMethod(t *testing.T) {
 			name: "Login method is PIN",
 			key: &registrymock.MockRegistryKey{SubKeys: []registrymock.MockRegistryKey{
 				{KeyName: "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Authentication\\LogonUI\\UserTile",
-					StringValues:         map[string]string{"S-1-5-21-174387295-310396468-1212757568-1001": "{D6886603-9D2F-4EB2-B667-1971041FA96B}"},
-					StatReturn:           &registry.KeyInfo{ValueCount: 1},
-					ReadValueNamesReturn: []string{"S-1-5-21-174387295-310396468-1212757568-1001", ""}, Err: nil}}},
+					StringValues: map[string]string{"S-1-5-21-174387295-310396468-1212757568-1001": "{D6886603-9D2F-4EB2-B667-1971041FA96B}"},
+					StatReturn:   &registry.KeyInfo{ValueCount: 1},
+					Err:          nil,
+				},
+			},
+			},
 			want: checks.NewCheckResult("LoginMethod", "PIN"),
 		},
 		{
 			name: "Login method is Picture",
 			key: &registrymock.MockRegistryKey{SubKeys: []registrymock.MockRegistryKey{
 				{KeyName: "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Authentication\\LogonUI\\UserTile",
-					StringValues:         map[string]string{"": "{2135F72A-90B5-4ED3-A7F1-8BB705AC276A}"},
-					StatReturn:           &registry.KeyInfo{ValueCount: 1},
-					ReadValueNamesReturn: []string{"", ""}, Err: nil}}},
+					StringValues: map[string]string{"": "{2135F72A-90B5-4ED3-A7F1-8BB705AC276A}"},
+					StatReturn:   &registry.KeyInfo{ValueCount: 1},
+					Err:          nil}}},
 			want: checks.NewCheckResult("LoginMethod", "Picture Logon"),
 		},
 		{
@@ -42,9 +45,8 @@ func TestLoginMethod(t *testing.T) {
 			key: &registrymock.MockRegistryKey{
 				SubKeys: []registrymock.MockRegistryKey{
 					{KeyName: "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Authentication\\LogonUI\\UserTile",
-						StringValues:         map[string]string{"S-1-5-21-174387295-310396468-1212757568-500": "{60B78E88-EAD8-445C-9CFD-0B87F74EA6CD}"},
-						StatReturn:           &registry.KeyInfo{ValueCount: 1},
-						ReadValueNamesReturn: []string{"S-1-5-21-174387295-310396468-1212757568-500", ""}, Err: nil}}},
+						StringValues: map[string]string{"S-1-5-21-174387295-310396468-1212757568-500": "{60B78E88-EAD8-445C-9CFD-0B87F74EA6CD}"},
+						StatReturn:   &registry.KeyInfo{ValueCount: 1}, Err: nil}}},
 			want: checks.NewCheckResult("LoginMethod", "Password"),
 		},
 		{
@@ -52,9 +54,8 @@ func TestLoginMethod(t *testing.T) {
 			key: &registrymock.MockRegistryKey{
 				SubKeys: []registrymock.MockRegistryKey{
 					{KeyName: "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Authentication\\LogonUI\\UserTile",
-						StringValues:         map[string]string{"": "{BEC09223-B018-416D-A0AC-523971B639F5}"},
-						StatReturn:           &registry.KeyInfo{ValueCount: 1},
-						ReadValueNamesReturn: []string{"", ""}, Err: nil}}},
+						StringValues: map[string]string{"": "{BEC09223-B018-416D-A0AC-523971B639F5}"},
+						StatReturn:   &registry.KeyInfo{ValueCount: 1}, Err: nil}}},
 			want: checks.NewCheckResult("LoginMethod", "Fingerprint"),
 		},
 		{
@@ -62,9 +63,8 @@ func TestLoginMethod(t *testing.T) {
 			key: &registrymock.MockRegistryKey{
 				SubKeys: []registrymock.MockRegistryKey{
 					{KeyName: "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Authentication\\LogonUI\\UserTile",
-						StringValues:         map[string]string{"": "{8AF662BF-65A0-4D0A-A540-A338A999D36F}"},
-						StatReturn:           &registry.KeyInfo{ValueCount: 1},
-						ReadValueNamesReturn: []string{"", ""}, Err: nil}}},
+						StringValues: map[string]string{"": "{8AF662BF-65A0-4D0A-A540-A338A999D36F}"},
+						StatReturn:   &registry.KeyInfo{ValueCount: 1}, Err: nil}}},
 			want: checks.NewCheckResult("LoginMethod", "Facial recognition"),
 		},
 		{
@@ -72,9 +72,8 @@ func TestLoginMethod(t *testing.T) {
 			key: &registrymock.MockRegistryKey{
 				SubKeys: []registrymock.MockRegistryKey{
 					{KeyName: "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Authentication\\LogonUI\\UserTile",
-						StringValues:         map[string]string{"": "{27FBDB57-B613-4AF2-9D7E-4FA7A66C21AD}"},
-						StatReturn:           &registry.KeyInfo{ValueCount: 1},
-						ReadValueNamesReturn: []string{"", ""}, Err: nil}}},
+						StringValues: map[string]string{"": "{27FBDB57-B613-4AF2-9D7E-4FA7A66C21AD}"},
+						StatReturn:   &registry.KeyInfo{ValueCount: 1}, Err: nil}}},
 			want: checks.NewCheckResult("LoginMethod", "Trust signal"),
 		},
 		{
@@ -82,9 +81,8 @@ func TestLoginMethod(t *testing.T) {
 			key: &registrymock.MockRegistryKey{
 				SubKeys: []registrymock.MockRegistryKey{
 					{KeyName: "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Authentication\\LogonUI\\UserTile",
-						StringValues:         map[string]string{"": "unknown"},
-						StatReturn:           &registry.KeyInfo{ValueCount: 1},
-						ReadValueNamesReturn: []string{"", ""}, Err: nil}}},
+						StringValues: map[string]string{"": "unknown"},
+						StatReturn:   &registry.KeyInfo{ValueCount: 1}, Err: nil}}},
 			want: checks.NewCheckResult("LoginMethod"),
 		},
 	}
