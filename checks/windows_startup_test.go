40f708948d390af1af251bfc9801150d7d969e97
Wouter99 <thiswouter@gmail.com>
2024-03-31 20:03:08 +0200

    to be continued

--- checks/windows_startup_test.go
+++ checks/windows_startup_test.go
@@ -23,14 +23,17 @@ func TestStartup(t *testing.T) {
 		want checks.Check
 	}{{
 		name: "No startup programs found",
-		key1: &registrymock.MockRegistryKey{SubKeys: []registrymock.MockRegistryKey{{KeyName: "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\StartupApproved\\Run"}}},
-		key2: &registrymock.MockRegistryKey{SubKeys: []registrymock.MockRegistryKey{{KeyName: "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\StartupApproved\\Run"}}},
-		key3: &registrymock.MockRegistryKey{SubKeys: []registrymock.MockRegistryKey{{KeyName: "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\StartupApproved\\Run32"}}},
+		// incorrect gebruik hkey_current_user etc. ?
+		key1: &registrymock.MockRegistryKey{SubKeys: []registrymock.MockRegistryKey{{KeyName: "HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\StartupApproved\\Run"}}},
+		key2: &registrymock.MockRegistryKey{SubKeys: []registrymock.MockRegistryKey{{KeyName: "HKEY_LOCAL_MACHINE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\StartupApproved\\Run"}}},
+		key3: &registrymock.MockRegistryKey{SubKeys: []registrymock.MockRegistryKey{{KeyName: "HKEY_LOCAL_MACHINE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\StartupApproved\\Run32"}}},
 		want: checks.NewCheckResult("Startup", "No startup programs found"),
 	}}
+	// meer unit tests voor: "error opening registry key", "error reading value names" en "startup programs found"
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := checks.Startup()
+			got := checks.Startup() // hier nog key in? (tt.key)
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("Startup() = %v, want %v", got, tt.want)
 			}
