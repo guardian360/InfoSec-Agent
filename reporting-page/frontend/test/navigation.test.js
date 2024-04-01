aad1b9072557b75509c22af4ab44549222c7df51
eimbens <e.imbens@gmail.com>
2024-04-01 18:40:09 +0200

    Add testing file for navigation menu

--- reporting-page/frontend/test/navigation.test.js
+++ reporting-page/frontend/test/navigation.test.js
@@ -1,124 +1,50 @@
 import test from 'unit.js';
-import { fillTable } from '../src/js/navigation-menu.js';
-import { sortTable } from '../src/js/navigation-menu.js';
 import { JSDOM } from "jsdom";
+import { MarkSelectedNavigationItem, CloseNavigation, ToggleNavigationHamburger, ToggleNavigationResize } from '../src/js/navigation-menu';
 
 // Mock page
 const dom = new JSDOM(`
-<!DOCTYPE html>
-<html>
-<body>
-  <table class="issues-table" id="issues-table">
-    <thead>
-      <tr>
-      <th class="issue-column"><span class="table-header">Name</span><span class="material-symbols-outlined" id="sort-on-issue">swap_vert</span></th>
-      <th class="type-column"><span class="table-header">Type</span><span class="material-symbols-outlined" id="sort-on-type">swap_vert</span></th>
-      <th class="risk-column"><span class="table-header">Risk level</span><span class="material-symbols-outlined" id="sort-on-risk">swap_vert</span></th>
-      </tr>
-    </thead>
-    <tbody>
-    </tbody>
-  </table>
-</body>
-</html>
 `);
 global.document = dom.window.document
 global.window = dom.window
 
 // Test cases
-describe('Issues table', function() {
-  it('should be filled with information from the provided JSON array', function() {
-    // Arrange input issues
-    let issues = [];
-    issues = [
-      { 
-        "Id": "Windows defender", 
-        "Result": ["Windows defender is disabled"],
-        "ErrorMSG": null
-      },
-      { 
-        "Id": "Camera and microphone access", 
-        "Result": ["Something has access to camera", "Something has access to microphone"],
-        "ErrorMSG": null
-      }
-    ];  
-    // Arrange expected table data
-    const expectedData = [
-      { 
-        "Name": "Windows defender", 
-        "Type": "Security",
-        "Risk": "High"
-      },
-      { 
-        "Name": "Camera and microphone access", 
-        "Type": "Privacy",
-        "Risk": "Low"
-      }
-    ];  
-    // Act
-    const tbody = global.document.querySelector('tbody');
-    fillTable(tbody, issues);  
-    // Assert
-    expectedData.forEach((expectedIssue, index) => {
-      const row = tbody.rows[index];
-      test.value(row.cells[0].textContent).isEqualTo(expectedData[index].Name);
-      test.value(row.cells[1].textContent).isEqualTo(expectedData[index].Type);
-      test.value(row.cells[2].textContent).isEqualTo(expectedData[index].Risk);
-    });
-  });  
-
-  it('should be sortable', function() {
-    // Arrange table rows
-    const table = global.document.getElementById("issues-table");
-    const tbody = table.querySelector('tbody');
-    tbody.innerHTML = `
-      <tr>
-        <td>Windows defender</td>
-        <td>Security</td>
-        <td>High</td>
-      </tr>
-      <tr>
-        <td>Camera and microphone access</td>
-        <td>Privacy</td>
-        <td>Low</td>
-      </tr>
-      <tr>
-        <td>Firewall settings</td>
-        <td>Security</td>
-        <td>Medium</td>
-      </tr>
-    `;  
+describe('MarkSelectedNavigationItem', function() {
+  it('should mark the item in the navigation menu that is selected', function() {
+    // Arrange
 
     // Act
-    sortTable(tbody, 0); 
 
     // Assert
-    let sortedRows = Array.from(tbody.rows);
-    let sortedNames = sortedRows.map(row => row.cells[0].textContent);
-    test.array(sortedNames).is(['Camera and microphone access', 'Firewall settings', 'Windows defender']);  
+  });
+});
+
+describe('CloseNavigation', function() {
+  it('should close the navigation menu if a menu item is selected and the screen size is smaller than 800 px', function() {
+    // Arrange
 
     // Act
-    sortTable(tbody, 0);  
 
     // Assert
-    let sortedRowsDescending = Array.from(tbody.rows);
-    let sortedNamesDescending = sortedRowsDescending.map(row => row.cells[0].textContent);
-    test.array(sortedNamesDescending).is(['Windows defender', 'Firewall settings', 'Camera and microphone access']);  
+  });
+});
+
+describe('ToggleNavigationHamburger', function() {
+  it('should open or close the navigation menu when the hamburger menu is clicked', function() {
+    // Arrange
 
     // Act
-    sortTable(tbody, 2);  
 
     // Assert
-    sortedRows = Array.from(tbody.rows);
-    let sortedRisks = sortedRows.map(row => row.cells[2].textContent);
-    test.array(sortedRisks).is(['High', 'Medium', 'Low']);  
+  });
+});
+
+describe('ToggleNavigationResize', function() {
+  it('should open or close the navigation menu when the screen is resized', function() {
+    // Arrange
 
     // Act
-    sortTable(tbody, 2);  
 
     // Assert
-    sortedRowsDescending = Array.from(tbody.rows);
-    let sortedRisksDescending = sortedRowsDescending.map(row => row.cells[2].textContent);
-    test.array(sortedRisksDescending).is(['Low', 'Medium', 'High']);
   });
 });
