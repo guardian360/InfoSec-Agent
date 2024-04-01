7b85a4faff6b4d19b8dfef8cb58d955eaac0e04e
SjaakRR <sjaak0110@gmail.com>
2024-04-01 19:04:00 +0200

    Fixed startup test

--- checks/windows_startup.go
+++ checks/windows_startup.go
@@ -21,7 +21,6 @@ func Startup(key1 registrymock.RegistryKey, key2 registrymock.RegistryKey, key3 
 		`SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StartupApproved\Run`)
 	lmKey2, err3 := registrymock.OpenRegistryKey(key3,
 		`SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StartupApproved\Run32`)
-
 	if err1 != nil || err2 != nil || err3 != nil {
 		return NewCheckError("Startup", fmt.Errorf("error opening registry keys"))
 	}
@@ -32,9 +31,9 @@ func Startup(key1 registrymock.RegistryKey, key2 registrymock.RegistryKey, key3 
 	defer registrymock.CloseRegistryKey(lmKey2)
 
 	// Read the entries within the registry key
-	cuValueNames, err1 := cuKey.ReadValueNames(0)
-	lmValueNames, err2 := lmKey.ReadValueNames(0)
-	lm2ValueNames, err3 := lmKey2.ReadValueNames(0)
+	cuValueNames, err1 := cuKey.ReadValueNames(10)
+	lmValueNames, err2 := lmKey.ReadValueNames(10)
+	lm2ValueNames, err3 := lmKey2.ReadValueNames(10)
 
 	if err1 != nil || err2 != nil || err3 != nil {
 		return NewCheckError("Startup", fmt.Errorf("error reading value names"))
--- checks/windows_startup_test.go
+++ checks/windows_startup_test.go
@@ -23,14 +23,14 @@ func TestStartup(t *testing.T) {
 	}{{
 		name: "No startup programs found",
 		key1: &registrymock.MockRegistryKey{SubKeys: []registrymock.MockRegistryKey{{KeyName: "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\StartupApproved\\Run"}}},
-		key2: &registrymock.MockRegistryKey{SubKeys: []registrymock.MockRegistryKey{{KeyName: "Microsoft\\Windows\\CurrentVersion\\Explorer\\StartupApproved\\Run"}}},
-		key3: &registrymock.MockRegistryKey{SubKeys: []registrymock.MockRegistryKey{{KeyName: "Microsoft\\Windows\\CurrentVersion\\Explorer\\StartupApproved\\Run32"}}},
+		key2: &registrymock.MockRegistryKey{SubKeys: []registrymock.MockRegistryKey{{KeyName: "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\StartupApproved\\Run"}}},
+		key3: &registrymock.MockRegistryKey{SubKeys: []registrymock.MockRegistryKey{{KeyName: "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\StartupApproved\\Run32"}}},
 		want: checks.NewCheckResult("Startup", "No startup programs found"),
 	}, {
 		name: "Startup programs found",
-		key1: &registrymock.MockRegistryKey{SubKeys: []registrymock.MockRegistryKey{{KeyName: "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\StartupApproved\\Run", BinaryValues: map[string][]byte{"MockProgram": []byte("0000")}, Err: nil}}},
-		key2: &registrymock.MockRegistryKey{SubKeys: []registrymock.MockRegistryKey{{KeyName: "Microsoft\\Windows\\CurrentVersion\\Explorer\\StartupApproved\\Run", BinaryValues: map[string][]byte{"MockProgram": []byte("0000")}, Err: nil}}},
-		key3: &registrymock.MockRegistryKey{SubKeys: []registrymock.MockRegistryKey{{KeyName: "Microsoft\\Windows\\CurrentVersion\\Explorer\\StartupApproved\\Run32", BinaryValues: map[string][]byte{"MockProgram": []byte("0000")}, Err: nil}}},
+		key1: &registrymock.MockRegistryKey{SubKeys: []registrymock.MockRegistryKey{{KeyName: "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\StartupApproved\\Run", BinaryValues: map[string][]byte{"MockProgram": []byte{1, 2, 3, 4, 0, 0, 0}}, Err: nil}}},
+		key2: &registrymock.MockRegistryKey{SubKeys: []registrymock.MockRegistryKey{{KeyName: "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\StartupApproved\\Run", BinaryValues: map[string][]byte{"MockProgram2": []byte{0, 0, 0, 0, 1, 0, 0}}, Err: nil}}},
+		key3: &registrymock.MockRegistryKey{SubKeys: []registrymock.MockRegistryKey{{KeyName: "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\StartupApproved\\Run32", BinaryValues: map[string][]byte{"MockProgram3": []byte{0, 0, 0, 0, 0, 1, 0}}, Err: nil}}},
 		want: checks.NewCheckResult("Startup", "MockProgram"),
 	}} /*,{
 		name: "Error finding startup programs",
