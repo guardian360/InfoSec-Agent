2db63c9a49f7515d6f7f18a567b424e1bca5e228
Knoop,K. (Kevin) <kefkefknoop@gmail.com>
2024-04-03 12:55:58 +0200

    Reduced complexity of smb_check_test.go function

--- checks/smb_check_test.go
+++ checks/smb_check_test.go
@@ -102,24 +102,19 @@ func TestSmbEnabled(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			var smbVersion string
 			if strings.Contains(tt.name, "SMB1") {
-				got, err := checks.SmbEnabled("SMB1", tt.executor)
-				if (err != nil) != tt.wantErr {
-					t.Errorf("SmbEnabled() error = %v, wantErr %v", err, tt.wantErr)
-					return
-				}
-				if got != tt.want {
-					t.Errorf("SmbEnabled() got = %v, want %v", got, tt.want)
-				}
+				smbVersion = "SMB1"
 			} else {
-				got, err := checks.SmbEnabled("SMB2", tt.executor)
-				if (err != nil) != tt.wantErr {
-					t.Errorf("SmbEnabled() error = %v, wantErr %v", err, tt.wantErr)
-					return
-				}
-				if got != tt.want {
-					t.Errorf("SmbEnabled() got = %v, want %v", got, tt.want)
-				}
+				smbVersion = "SMB2"
+			}
+			got, err := checks.SmbEnabled(smbVersion, tt.executor)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("SmbEnabled() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("SmbEnabled() got = %v, want %v", got, tt.want)
 			}
 		})
 	}
