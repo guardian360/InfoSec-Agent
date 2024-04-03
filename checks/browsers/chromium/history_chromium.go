0ac6918fe123648be2268ffb5b402624b2149138
Knoop,K. (Kevin) <kefkefknoop@gmail.com>
2024-04-03 13:23:01 +0200

    Reduced cognitive complexity of Browser history functions

--- checks/browsers/chromium/history_chromium.go
+++ checks/browsers/chromium/history_chromium.go
@@ -2,33 +2,31 @@ package chromium
 
 import (
 	"database/sql"
+	"github.com/InfoSec-Agent/InfoSec-Agent/utils"
 	"log"
 	"os"
 	"path/filepath"
 	"regexp"
-	"strings"
-	"time"
 
 	"github.com/InfoSec-Agent/InfoSec-Agent/checks"
-	"github.com/InfoSec-Agent/InfoSec-Agent/utils"
 	// Necessary to use the sqlite driver
 	_ "modernc.org/sqlite"
+	"strings"
+	"time"
 )
 
 // HistoryChromium checks the user's history in a Chromium based browser for phishing domains.
 //
 // Parameters:
 //
-//	browser (string) - The name of the browser to check
+// browser (string) - The name of the browser to check
 //
 // Returns: The phishing domains that the user has visited in the last week and when they visited it
 func HistoryChromium(browser string) checks.Check {
-	// List of the results, this will contain a list of domains which are known to be phishing domains.
 	var results []string
 	var browserPath string
 	var returnBrowserName string
-	// Set the browser path and the return browser name based on the browser to check
-	// Currently, supports checking of Google Chrome and Microsoft Edge
+
 	if browser == chrome {
 		returnBrowserName = "HistoryChrome"
 		browserPath = chromePath
@@ -37,7 +35,6 @@ func HistoryChromium(browser string) checks.Check {
 		returnBrowserName = "HistoryEdge"
 		browserPath = edgePath
 	}
-
 	// Get the current user's home directory, where the history can be found
 	user, err := os.UserHomeDir()
 	if err != nil {
@@ -66,49 +63,66 @@ func HistoryChromium(browser string) checks.Check {
 	if err != nil {
 		return checks.NewCheckError(returnBrowserName, err)
 	}
-	defer func(db *sql.DB) {
-		err = db.Close()
-		if err != nil {
-			log.Println("error closing database: ", err)
-		}
-	}(db)
+	defer closeDatabase(db)
 
-	// Get the time of one week ago (with Chrome the counting starts from 1601-01-01)
+	rows, err := queryDatabase(db)
+	if err != nil {
+		return checks.NewCheckError(returnBrowserName, err)
+	}
+	defer closeRows(rows)
+
+	results, err = processQueryResults(rows)
+	if err != nil {
+		return checks.NewCheckError(returnBrowserName, err)
+	}
+
+	if len(results) > 0 {
+		return checks.NewCheckResult(returnBrowserName, strings.Join(results, "\n"))
+	}
+	return checks.NewCheckResult(returnBrowserName, "No phishing domains found in the last week")
+}
+
+func closeDatabase(db *sql.DB) {
+	if err := db.Close(); err != nil {
+		log.Println("error closing database: ", err)
+	}
+}
+
+func queryDatabase(db *sql.DB) (*sql.Rows, error) {
 	oneWeekAgo := time.Now().AddDate(369, 0, -7).UnixMicro()
-	// Query the history table
-	// We limit the results to 50 for the time being
 	rows, err := db.Query(
 		"SELECT url, title, visit_count, last_visit_time FROM urls "+
 			"WHERE last_visit_time > ? ORDER BY last_visit_time DESC", oneWeekAgo)
 	if err != nil {
-		return checks.NewCheckError(returnBrowserName, err)
+		return nil, err
 	}
-	defer func(rows *sql.Rows) {
-		err = rows.Close()
-		if err != nil {
-			log.Println("error closing rows: ", err)
-		}
-	}(rows)
 
-	// Get the phishing domains from an up-to-date GitHub list
+	return rows, nil
+}
+
+func closeRows(rows *sql.Rows) {
+	if err := rows.Close(); err != nil {
+		log.Println("error closing rows: ", err)
+	}
+}
+
+func processQueryResults(rows *sql.Rows) ([]string, error) {
+	var results []string
 	phishingDomainList := utils.GetPhishingDomains()
-	// Compare the visited domains with the phishing domains
+	re := regexp.MustCompile(`(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+\.[^:\/\n?]+)`)
+
 	for rows.Next() {
 		var url, title string
 		var visitCount, lastVisitTime int
-		err = rows.Scan(&url, &title, &visitCount, &lastVisitTime)
+		err := rows.Scan(&url, &title, &visitCount, &lastVisitTime)
 		if err != nil {
-			return checks.NewCheckError(returnBrowserName, err)
+			return nil, err
 		}
-		// The following regex is used to extract the domain from the url,
-		// to use for mapping against the phishing domains
-		re := regexp.MustCompile(`(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+\.[^:\/\n?]+)`)
-		matches := re.FindStringSubmatch(url)
 
+		matches := re.FindStringSubmatch(url)
 		for _, scamDomain := range phishingDomainList {
 			if len(matches) > 1 && matches[1] == scamDomain {
 				domain := matches[1]
-				// Return which domain is associated with phishing and when it was visited
 				results = append(results, "You visited website: "+domain+" which is a known phishing domain. "+
 					"The time of the last visit: "+
 					""+time.UnixMicro(int64(lastVisitTime)).AddDate(-369, 0, 0).String())
@@ -116,14 +130,9 @@ func HistoryChromium(browser string) checks.Check {
 		}
 	}
 
-	// Check for errors from iterating over the database rows
-	if err = rows.Err(); err != nil {
-		return checks.NewCheckError(returnBrowserName, err)
+	if err := rows.Err(); err != nil {
+		return nil, err
 	}
 
-	// Return the domains found, and when they were visited
-	if len(results) > 0 {
-		return checks.NewCheckResult(returnBrowserName, strings.Join(results, "\n"))
-	}
-	return checks.NewCheckResult(returnBrowserName, "No phishing domains found in the last week")
+	return results, nil
 }
--- checks/browsers/firefox/history_firefox.go
+++ checks/browsers/firefox/history_firefox.go
@@ -9,8 +9,8 @@ import (
 	"regexp"
 	"time"
 
-	// Necessary to use the sqlite driver
 	"github.com/InfoSec-Agent/InfoSec-Agent/checks"
+	// Necessary to use the sqlite driver
 	_ "modernc.org/sqlite"
 )
 
@@ -21,7 +21,28 @@ import (
 // Returns: The phishing domains that the user has visited in the last week and when they visited it
 func HistoryFirefox() checks.Check {
 	var output []string
-	db, err := setupDatabase()
+	ffdirectory, err := utils.FirefoxFolder()
+	if err != nil {
+		return checks.NewCheckErrorf("HistoryFirefox", "No firefox directory found", err)
+	}
+
+	// Copy the database, so we don't have problems with locked files
+	tempHistoryDbff := filepath.Join(os.TempDir(), "tempHistoryDb.sqlite")
+	// Clean up the temporary file when the function returns
+	defer func(name string) {
+		err = os.Remove(name)
+		if err != nil {
+			log.Println("error removing file: ", err)
+		}
+	}(tempHistoryDbff)
+
+	// Copy the database to a temporary location
+	copyError := utils.CopyFile(ffdirectory[0]+"\\places.sqlite", tempHistoryDbff)
+	if copyError != nil {
+		return checks.NewCheckError("HistoryFirefox", copyError)
+	}
+
+	db, err := sql.Open("sqlite", tempHistoryDbff)
 	if err != nil {
 		return checks.NewCheckError("HistoryFirefox", err)
 	}
@@ -39,39 +60,6 @@ func HistoryFirefox() checks.Check {
 	}
 
 	return checks.NewCheckResult("HistoryFirefox", output...)
-}
-
-// setupDatabase is a helper function used by the HistoryFirefox function.
-// It sets up a temporary copy of the Firefox history database for querying.
-//
-// Parameters: _
-//
-// Returns: A pointer to the database and an error if applicable
-func setupDatabase() (*sql.DB, error) {
-	ffdirectory, err := utils.FirefoxFolder()
-	if err != nil {
-		return nil, err
-	}
-
-	tempHistoryDbff := filepath.Join(os.TempDir(), "tempHistoryDb.sqlite")
-	defer func(name string) {
-		err = os.Remove(name)
-		if err != nil {
-			log.Println("error removing file: ", err)
-		}
-	}(tempHistoryDbff)
-
-	err = utils.CopyFile(ffdirectory[0]+"\\places.sqlite", tempHistoryDbff)
-	if err != nil {
-		return nil, err
-	}
-
-	db, err := sql.Open("sqlite", tempHistoryDbff)
-	if err != nil {
-		return nil, err
-	}
-
-	return db, nil
 }
 
 // closeDatabase is a helper function used by the HistoryFirefox function.
@@ -135,10 +123,7 @@ func processQueryResults(rows *sql.Rows) ([]string, error) {
 			return nil, err
 		}
 
-		timeString, err := formatTime(lastVisitDate)
-		if err != nil {
-			return nil, err
-		}
+		timeString := formatTime(lastVisitDate)
 
 		matches := re.FindStringSubmatch(url)
 		for _, scamDomain := range phishingDomainList {
@@ -157,8 +142,8 @@ func processQueryResults(rows *sql.Rows) ([]string, error) {
 //
 // Parameters: lastVisitDate (sql.NullInt64) - the last visit date of a website
 //
-// Returns: The last visit date of a website as a human-readable string and an error if applicable
-func formatTime(lastVisitDate sql.NullInt64) (string, error) {
+// Returns: The last visit date of a website as a human-readable string
+func formatTime(lastVisitDate sql.NullInt64) string {
 	var lastVisitDateInt64 int64
 	if lastVisitDate.Valid {
 		lastVisitDateInt64 = lastVisitDate.Int64
@@ -168,105 +153,8 @@ func formatTime(lastVisitDate sql.NullInt64) (string, error) {
 
 	if lastVisitDateInt64 > 0 {
 		timeofCreation := time.UnixMicro(lastVisitDateInt64)
-		return timeofCreation.String(), nil
-	} else {
-		var timeNow = time.Now()
-		return timeNow.String(), nil
+		return timeofCreation.String()
 	}
+	var timeNow = time.Now()
+	return timeNow.String()
 }
-
-//func HistoryFirefox() checks.Check {
-//	var output []string
-//	ffdirectory, err := utils.FirefoxFolder()
-//	if err != nil {
-//		return checks.NewCheckErrorf("HistoryFirefox", "No firefox directory found", err)
-//	}
-//
-//	// Copy the database, so we don't have problems with locked files
-//	tempHistoryDbff := filepath.Join(os.TempDir(), "tempHistoryDb.sqlite")
-//	// Clean up the temporary file when the function returns
-//	defer func(name string) {
-//		err = os.Remove(name)
-//		if err != nil {
-//			log.Println("error removing file: ", err)
-//		}
-//	}(tempHistoryDbff)
-//
-//	// Copy the database to a temporary location
-//	copyError := utils.CopyFile(ffdirectory[0]+"\\places.sqlite", tempHistoryDbff)
-//	if copyError != nil {
-//		return checks.NewCheckError("HistoryFirefox", copyError)
-//	}
-//
-//	db, err := sql.Open("sqlite", tempHistoryDbff)
-//	if err != nil {
-//		return checks.NewCheckError("HistoryFirefox", err)
-//	}
-//	defer func(db *sql.DB) {
-//		err = db.Close()
-//		if err != nil {
-//			log.Println("error closing database: ", err)
-//		}
-//	}(db)
-//
-//	lastWeek := time.Now().AddDate(0, 0, -7).UnixMicro()
-//
-//	// Get the phishing domains from up-to-date GitHub list
-//	phishingDomainList := utils.GetPhishingDomains()
-//
-//	// Query the urls and when the sites were visited from the history database
-//	rows, err := db.Query(
-//		"SELECT url, last_visit_date FROM moz_places WHERE last_visit_date >= ? ORDER BY last_visit_date DESC",
-//		lastWeek)
-//	// TODO: check if this is error handling is correct
-//	if rows.Err() != nil {
-//		return checks.NewCheckError("HistoryFirefox", rows.Err())
-//	}
-//	if err != nil {
-//		return checks.NewCheckError("HistoryFirefox", err)
-//	}
-//	defer func(rows *sql.Rows) {
-//		err = rows.Close()
-//		if err != nil {
-//			log.Println("error closing rows: ", err)
-//		}
-//	}(rows)
-//
-//	// Iterate over each found url
-//	for rows.Next() {
-//		var url string
-//		var lastVisitDate sql.NullInt64
-//		// Scan the row into variables
-//		if err = rows.Scan(&url, &lastVisitDate); err != nil {
-//			return checks.NewCheckError("HistoryFirefox", err)
-//		}
-//		var timeString string
-//		// Check if the lastVisitDate is nil
-//		var lastVisitDateInt64 int64
-//		if lastVisitDate.Valid {
-//			lastVisitDateInt64 = lastVisitDate.Int64
-//		} else {
-//			lastVisitDateInt64 = -1 // Default value
-//		}
-//		if lastVisitDateInt64 > 0 {
-//			timeofCreation := time.UnixMicro(lastVisitDateInt64)
-//			timeString = timeofCreation.String()
-//		} else {
-//			var time = time.Now()
-//			timeString = time.String()
-//		}
-//
-//		// The following regex is used to extract the domain from the url,
-//		// to use for mapping against the phishing domains
-//		re := regexp.MustCompile(`(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+\.[^:\/\n?]+)`)
-//		matches := re.FindStringSubmatch(url)
-//
-//		for _, scamDomain := range phishingDomainList {
-//			if len(matches) > 1 && matches[1] == scamDomain {
-//				domain := matches[1]
-//				output = append(output, domain+timeString)
-//			}
-//		}
-//	}
-//	return checks.NewCheckResult("HistoryFirefox", output...)
-//}
