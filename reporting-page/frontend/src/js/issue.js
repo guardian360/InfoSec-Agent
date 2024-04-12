858c118304eddbc1ded0fbb2aeebed7079613b08
floerer <flovlist1@gmail.com>
2024-04-12 13:10:36 +0200

    Fixed database.json and changed view for acceptable issues

--- reporting-page/frontend/src/js/issue.js
+++ reporting-page/frontend/src/js/issue.js
@@ -54,45 +54,72 @@ export function previousSolutionStep(solutionText, solutionScreenshot, solution,
 export function openIssuePage(issueId) {
   stepCounter = 0;
   const currentIssue = data[issueId];
-  const pageContents = document.getElementById('page-contents');
-  pageContents.innerHTML = `
-    <h1 class="issue-name">${currentIssue.Name}</h1>
-    <div class="issue-information">
-      <h2 id="information">Information</h2>
-      <p>${currentIssue.Information}</p>
-      <h2 id="solution">Solution</h2>
-      <div class="issue-solution">
-        <p id="solution-text">${currentIssue.Solution[stepCounter]}</p>
-        <img style='display:block; width:750px;height:auto' id="step-screenshot"></img>
-        <div class="solution-buttons">
-          <div class="button-box">
-            <div id="previous-button" class="step-button">&laquo; Previous step</div>
-            <div id="next-button" class="step-button">Next step &raquo;</div>
+  if (currentIssue.Screenshots.length === 0) {
+    const pageContents = document.getElementById('page-contents');
+    pageContents.innerHTML = `
+      <h1 class="issue-name">${currentIssue.Name}</h1>
+      <div class="issue-information">
+        <h2 id="information">Information</h2>
+        <p>${currentIssue.Information}</p>
+        <h2 id="solution">Solution</h2>
+        <div class="issue-solution">
+          <p id="solution-text">${currentIssue.Solution[stepCounter]}</p>
+        </div>
+      </div>
+      <div id="back-button">Back to issues overview</div>
+    `;
+
+    const texts = ['information', 'solution', 'previous-button', 'next-button', 'back-button'];
+    const localizationIds = ['Issues.Information', 'Issues.Solution', 'Issues.Previous', 'Issues.Next', 'Issues.Back'];
+    for (let i = 0; i < texts.length; i++) {
+      getLocalization(localizationIds[i], texts[i]);
+    }
+    const solutionText = document.getElementById('solution-text');
+
+    document.getElementById('back-button').addEventListener('click', () => openIssuesPage());
+    document.onload = retrieveTheme();
+  }
+  else {
+    const pageContents = document.getElementById('page-contents');
+    pageContents.innerHTML = `
+      <h1 class="issue-name">${currentIssue.Name}</h1>
+      <div class="issue-information">
+        <h2 id="information">Information</h2>
+        <p>${currentIssue.Information}</p>
+        <h2 id="solution">Solution</h2>
+        <div class="issue-solution">
+          <p id="solution-text">${currentIssue.Solution[stepCounter]}</p>
+          <img style='display:block; width:750px;height:auto' id="step-screenshot"></img>
+          <div class="solution-buttons">
+            <div class="button-box">
+              <div id="previous-button" class="step-button">&laquo; Previous step</div>
+              <div id="next-button" class="step-button">Next step &raquo;</div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <div id="back-button">Back to issues overview</div>
-  `;
+      <div id="back-button">Back to issues overview</div>
+    `;
 
-  const texts = ['information', 'solution', 'previous-button', 'next-button', 'back-button'];
-  const localizationIds = ['Issues.Information', 'Issues.Solution', 'Issues.Previous', 'Issues.Next', 'Issues.Back'];
-  for (let i = 0; i < texts.length; i++) {
-    getLocalization(localizationIds[i], texts[i]);
+    const texts = ['information', 'solution', 'previous-button', 'next-button', 'back-button'];
+    const localizationIds = ['Issues.Information', 'Issues.Solution', 'Issues.Previous', 'Issues.Next', 'Issues.Back'];
+    for (let i = 0; i < texts.length; i++) {
+      getLocalization(localizationIds[i], texts[i]);
+    }
+
+    try {
+      document.getElementById('step-screenshot').src = currentIssue.Screenshots[stepCounter];
+    } catch (error) { }
+
+    // Add functions to page for navigation
+    const solutionText = document.getElementById('solution-text');
+    const solutionScreenshot = document.getElementById('step-screenshot');
+    document.getElementById('next-button').addEventListener('click', () =>
+      nextSolutionStep(solutionText, solutionScreenshot, currentIssue.Solution, currentIssue.Screenshots));
+    document.getElementById('previous-button').addEventListener('click', () =>
+      previousSolutionStep(solutionText, solutionScreenshot, currentIssue.Solution, currentIssue.Screenshots));
+    document.getElementById('back-button').addEventListener('click', () => openIssuesPage());
+
+    document.onload = retrieveTheme();
   }
-
-  try {
-    document.getElementById('step-screenshot').src = currentIssue.Screenshots[stepCounter];
-  } catch (error) { }
-
-  // Add functions to page for navigation
-  const solutionText = document.getElementById('solution-text');
-  const solutionScreenshot = document.getElementById('step-screenshot');
-  document.getElementById('next-button').addEventListener('click', () =>
-    nextSolutionStep(solutionText, solutionScreenshot, currentIssue.Solution, currentIssue.Screenshots));
-  document.getElementById('previous-button').addEventListener('click', () =>
-    previousSolutionStep(solutionText, solutionScreenshot, currentIssue.Solution, currentIssue.Screenshots));
-  document.getElementById('back-button').addEventListener('click', () => openIssuesPage());
-
-  document.onload = retrieveTheme();
 }
