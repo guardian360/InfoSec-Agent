ea8eaa3182e06fce06f3e7ee57b18a883a77980c
LarsSlijkoord <l.x.slijkoord@students.uu.nl>
2024-04-22 10:46:41 +0200

    looking into mocha config

--- reporting-page/frontend/src/js/personalize.js
+++ reporting-page/frontend/src/js/personalize.js
@@ -1,6 +1,6 @@
 import {closeNavigation} from './navigation-menu.js';
 import {loadPersonalizeNavigation} from './navigation-menu.js';
-import logoPhoto from '../assets/images/logoTeamA-transformed.png';
+// import logoPhoto from '../assets/images/logoTeamA-transformed.png';
 
 /** Load the content of the Personalize page */
 export function openPersonalizePage() {
@@ -204,6 +204,7 @@ export function retrieveTheme() {
  */
 export function resetSettings() {
   localStorage.clear();
+  logoPhoto = "frontend/src/assets/images/logoTeamA-transformed.png";
   const logo = document.getElementById('logo');
   logo.src = logoPhoto;
 
