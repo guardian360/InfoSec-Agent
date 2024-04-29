6b4cd2dfa23691ad56aa3f82b97249d804ccee28
floerer <flovlist1@gmail.com>
2024-04-29 13:32:42 +0200

    added tests for SMB enabled

--- checks/smb_check_test.go
+++ checks/smb_check_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// TestCheckSMB is a function that tests the SmbEnabled function's behavior with various inputs.
+// TestSMBCheck is a function that tests the SmbCheck function's behavior with various inputs.
 //
 // Parameters:
 //   - t *testing.T: The testing framework provided by the Go testing package. It provides methods for reporting test failures and logging additional information.
@@ -17,7 +17,7 @@ import (
 // Returns: None
 //
 // This function tests the SmbEnabled function with different scenarios. It uses a mock implementation of the CommandExecutor interface to simulate the behavior of the command execution for checking the status of SMB1 and SMB2 protocols. Each test case checks if the SmbEnabled function correctly identifies the status of the SMB protocols based on the simulated command output. The function asserts that the returned string and error match the expected results.
-func TestCheckSMB(t *testing.T) {
+func TestSMBCheck(t *testing.T) {
 	tests := []struct {
 		name     string
 		executor *mocking.MockCommandExecutor
@@ -63,6 +63,66 @@ func TestCheckSMB(t *testing.T) {
 			} else {
 				require.Equal(t, tt.want, got)
 			}
+		})
+	}
+}
+
+// TestSmbEnabled is a function that tests the SmbEnabled function's behavior with various inputs.
+//
+// Parameters:
+//   - t *testing.T: The testing framework provided by the Go testing package. It provides methods for reporting test failures and logging additional information.
+//
+// Returns: None
+func TestSmbEnabled(t *testing.T) {
+	tests := []struct {
+		name     string
+		executor *mocking.MockCommandExecutor
+		SMB1     string
+		SMB2     string
+		resultID int
+	}{
+		{
+			name:     "SMB1 and SMB2 enabled",
+			executor: &mocking.MockCommandExecutor{Output: "True True", Err: nil},
+			SMB1:     "SMB1: enabled",
+			SMB2:     "SMB2: enabled",
+			resultID: 3,
+		},
+		{
+			name:     "Only SMB1 enabled",
+			executor: &mocking.MockCommandExecutor{Output: "True False", Err: nil},
+			SMB1:     "SMB1: enabled",
+			SMB2:     "SMB2: not enabled",
+			resultID: 1,
+		},
+		{
+			name:     "Only SMB2 enabled",
+			executor: &mocking.MockCommandExecutor{Output: "False True", Err: nil},
+			SMB1:     "SMB1: not enabled",
+			SMB2:     "SMB2: enabled",
+			resultID: 2,
+		},
+		{
+			name:     "Neither SMB1 nor SMB2 enabled",
+			executor: &mocking.MockCommandExecutor{Output: "False False", Err: nil},
+			SMB1:     "SMB1: not enabled",
+			SMB2:     "SMB2: not enabled",
+			resultID: 0,
+		},
+		{
+			name:     "command error",
+			executor: &mocking.MockCommandExecutor{Output: "", Err: errors.New("command error")},
+			SMB1:     "",
+			SMB2:     "",
+			resultID: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotSMB1, gotSMB2, gotResultID, _ := checks.SmbEnabled(tt.executor, 0)
+			require.Equal(t, tt.SMB1, gotSMB1)
+			require.Equal(t, tt.SMB2, gotSMB2)
+			require.Equal(t, tt.resultID, gotResultID)
 		})
 	}
 }
