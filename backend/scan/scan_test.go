9ebcde5e9b502e3907a137d7ece001271d4b91db
eimbens <e.imbens@gmail.com>
2024-05-15 12:20:51 +0200

    Update documentation

--- backend/scan/scan_test.go
+++ backend/scan/scan_test.go
@@ -10,10 +10,10 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// TestGetSeverity tests the GetSeverity function to ensure it returns the correct severity level for a given check ID.
+// TestGetSeverity tests the GetSeverity function to ensure it returns the correct severity level for a given issue ID and result ID pair.
 //
-// This test function creates a new SQLite database connection and calls the GetSeverity function with known check IDs.
-// It then asserts that the returned severity level matches the expected value for each check ID.
+// This test function creates a new SQLite database connection and calls the GetSeverity function with known issue IDs and result IDs.
+// It then asserts that the returned severity level matches the expected value for each issue ID and result ID pair.
 //
 // Parameters:
 //   - t *testing.T: The testing framework used for assertions.
@@ -49,10 +49,10 @@ func TestGetSeverity(t *testing.T) {
 	require.Equal(t, sql.ErrNoRows.Error(), err.Error())
 }
 
-// TestGetJSONKey tests the GetJSONKey function to ensure it returns the correct JSON key for a given check ID.
+// TestGetJSONKey tests the GetJSONKey function to ensure it returns the correct JSON key for a given issue ID and result ID pair.
 //
-// This test function creates a new SQLite database connection and calls the GetJSONKey function with known check IDs.
-// It then asserts that the returned JSON key matches the expected value for each check ID.
+// This test function creates a new SQLite database connection and calls the GetJSONKey function with known issue IDs and result IDs.
+// It then asserts that the returned JSON key matches the expected value for each issue ID and result ID pair.
 //
 // Parameters:
 //   - t *testing.T: The testing framework used for assertions.
@@ -89,6 +89,15 @@ func TestGetJSONKey(t *testing.T) {
 	require.Equal(t, sql.ErrNoRows.Error(), err.Error())
 }
 
+// TestGetDataBaseData tests the GetDataBaseData function to ensure it returns the correct database data for a given list of checks.
+//
+// This test function creates a list of check results and calls the GetDataBaseData function.
+// It then asserts that the returned database data matches the expected data for the given checks.
+//
+// Parameters:
+//   - t *testing.T: The testing framework used for assertions.
+//
+// No return values.
 func TestGetDataBaseData(t *testing.T) {
 	logger.SetupTests()
 
