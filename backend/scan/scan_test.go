b6ba8f8a63c5845b9ed99863cdf2c0cbd248bcc8
eimbens <e.imbens@gmail.com>
2024-05-15 11:17:21 +0200

    Add GetJSONKey unit test

--- backend/scan/scan_test.go
+++ backend/scan/scan_test.go
@@ -18,22 +18,56 @@ import (
 //
 // No return values.
 func TestGetSeverity(t *testing.T) {
+	testCases := []struct {
+		issueID          int
+		resultID         int
+		expectedSeverity int
+	}{
+		{1, 1, 4},
+		{3, 0, 1},
+	}
 	db, err := sql.Open("sqlite", "../../reporting-page/database.db")
 	if err != nil {
 		t.Errorf("Error occurred: %v", err)
 	}
 
-	// Test case where the severity level is 0
-	severity, err := scan.GetSeverity(db, 1, 1)
-	if err != nil {
-		t.Errorf("Error occurred: %v", err)
+	for _, tc := range testCases {
+		severity, err := scan.GetSeverity(db, tc.issueID, tc.resultID)
+		if err != nil {
+			t.Errorf("Error occurred: %v", err)
+		}
+		require.Equal(t, tc.expectedSeverity, severity)
 	}
-	require.Equal(t, 4, severity)
+}
 
-	// Test case where the severity level is 1
-	severity, err = scan.GetSeverity(db, 3, 0)
+// TestGetJSONKey tests the GetJSONKey function to ensure it returns the correct JSON key for a given check ID.
+//
+// This test function creates a new SQLite database connection and calls the GetJSONKey function with known check IDs.
+// It then asserts that the returned JSON key matches the expected value for each check ID.
+//
+// Parameters:
+//   - t *testing.T: The testing framework used for assertions.
+//
+// No return values.
+func TestGetJSONKey(t *testing.T) {
+	testCases := []struct {
+		issueID         int
+		resultID        int
+		expectedJSONKey int
+	}{
+		{1, 1, 11},
+		{3, 0, 30},
+	}
+	db, err := sql.Open("sqlite", "../../reporting-page/database.db")
 	if err != nil {
 		t.Errorf("Error occurred: %v", err)
 	}
-	require.Equal(t, 1, severity)
+
+	for _, tc := range testCases {
+		jsonKey, err := scan.GetJSONKey(db, tc.issueID, tc.resultID)
+		if err != nil {
+			t.Errorf("Error occurred: %v", err)
+		}
+		require.Equal(t, tc.expectedJSONKey, jsonKey)
+	}
 }
