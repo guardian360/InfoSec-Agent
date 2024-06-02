eaf779e0f11a984d01b7bf155685c830e456f0ac
floerer <flovlist1@gmail.com>
2024-06-02 21:54:57 +0200

    Added new page for programs and added localization

--- reporting-page/frontend/src/js/programs.js
+++ reporting-page/frontend/src/js/programs.js
@@ -1,13 +1,7 @@
-import {PieChart} from './piechart.js';
 import {getLocalization} from './localize.js';
 import {closeNavigation, markSelectedNavigationItem} from './navigation-menu.js';
 import {retrieveTheme} from './personalize.js';
-import {scanTest} from './database.js';
 import {LogError as logError} from '../../wailsjs/go/main/Tray.js';
-import {openIssuePage} from './issue.js';
-import {saveProgress, shareProgress, selectSocialMedia} from './share.js';
-import data from '../databases/database.en-GB.json' assert { type: 'json' };
-import {showModal} from './settings.js';
 
 /** Load the content of the Home page */
 export function openProgramsPage() {
@@ -17,23 +11,18 @@ export function openProgramsPage() {
     sessionStorage.setItem('savedPage', 1);
 
     document.getElementById('page-contents').innerHTML = `
-    <div class="issues-data">
-        <div class="table-container">
-        <h2 class="lang-issue-table"></h2>
-        <table class="issues-table" id="issues-table">
+    <div class="program-data">
+        <div class="program-container">
+        <h2 class="lang-program-table"></h2>
+        <table class="program-table" id="program-table">
             <thead>
             <tr>
-            <th class="issue-column">
+            <th class="program-column">
                 <span class="table-header lang-name"></span>
                 <span class="material-symbols-outlined" id="sort-on-issue">swap_vert</span>
             </th>
-            <th class="type-column">
-                <span class="table-header lang-type"></span>
-                <span class="material-symbols-outlined" id="sort-on-type">swap_vert</span>
-            </th>
-            <th class="risk-column">
-                <span class="table-header lang-risk"></span>
-                <span class="material-symbols-outlined" id="sort-on-risk">swap_vert</span>
+            <th class="version-column">
+                <span class="table-header"></span>
             </th>
             </tr>
             </thead>
@@ -44,9 +33,17 @@ export function openProgramsPage() {
     </div>
     `;
 
-    for (let i = 0; i < staticHomePageContent.length; i++) {
-        getLocalization(localizationIds[i], staticHomePageContent[i]);
-    }
+    const tableHeaders = [
+        'lang-program-table',
+        'lang-name',
+      ];
+      const localizationIds = [
+        'Programs.ProgramTable',
+        'Programs.Name',
+      ];
+      for (let i = 0; i < tableHeaders.length; i++) {
+        getLocalization(localizationIds[i], tableHeaders[i]);
+      }
 }
 
 /* istanbul ignore next */
