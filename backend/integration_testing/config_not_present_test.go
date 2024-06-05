a77d2b21276ec6d0040021ce9f57c48321233f7d
Knoop,K. (Kevin) <kefkefknoop@gmail.com>
2024-06-05 16:03:18 +0200

    More robust check for installed browsers

--- backend/integration_testing/config_not_present_test.go
+++ backend/integration_testing/config_not_present_test.go
@@ -7,15 +7,6 @@ import (
 
 var testsNotPresent = []func(t *testing.T){
 	i.TestIntegrationFirefoxFolderNotExists,
-	// TODO: turn back on when the test is fixed
-	// i.TestIntegrationExtensionsChromiumNotInstalled,
-	// TODO: turn back on when the test is fixed
-	// i.TestIntegrationHistoryChromiumNotInstalled,
-	// TODO: turn back on when the test is fixed
-	// i.TestIntegrationSearchEngineChromiumNotInstalled,
-	i.TestIntegrationSearchEngineFirefoxNotInstalled,
-	i.TestIntegrationHistoryFirefoxNotInstalled,
-	i.TestIntegrationExtensionsFirefoxNotInstalled,
 	i.TestIntegrationLoginMethodPasswordAndPIN,
 	// TODO: turn back on when the test is fixed
 	// i.TestIntegrationOutdatedWin10UpToDate,
